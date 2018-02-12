24816547c4b3fd5507df3de2e790e54069e9ffec
TalhaTZA <talavjtza@gmail.com>
2018-02-12 21:40:26 +0500

    ADD passport strategy of jwt

---
+++ server/services/passport.js
@@ -0,0 +1,24 @@
+const passport=require('passport');
+const User=require('../models/user_model');
+const config=require('../config');
+const JwtStrategy=require('passport-jwt').Strategy;
+const ExtractJwt=require('passport-jwt').ExtractJwt;
+
+const jwtOptions={
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey:config.secret
+};
+
+const jwtLogin=new JwtStrategy(jwtOptions,function(payload,done){
+
+    User.findById(payload.sub,function (err,user) {
+        if(err){ return done(err,false)};
+
+        if(user){ return done(null,user);}
+        
+        else{ return done(null,false);}
+    
+    });
+});
+
+passport.use(jwtLogin);
