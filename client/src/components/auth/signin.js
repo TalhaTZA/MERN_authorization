cd22797befbc43c6034e2755123f1d4e25126edd
TalhaTZA <talavjtza@gmail.com>
2018-04-06 18:27:17 +0500

    ADD form component with redux-form

---
+++ client/src/components/auth/signin.js
@@ -0,0 +1,34 @@
+import React , {Component} from 'react';
+import {reduxForm} from 'redux-form';
+
+class SignIn extends Component{
+
+    formSubmit({email,password}){
+        console.log(email,password);
+    }
+
+    render(){
+
+        const {fields:{ email , password } , handleSubmit } = this .props;
+
+        return(
+            <form onSubmit={handleSubmit(this.formSubmit.bind(this))}>
+                <fieldset className='form-group'>
+                    <label>Email:</label>
+                    <input {...email} className='form-control'/>
+                </fieldset>
+                <fieldset className='form-group'>
+                    <label>Password:</label>
+                    <input {...password} className='form-control'/>
+                </fieldset>
+                <button action='submit' className='btn btn-primary'>Sign In</button>
+            </form>
+        );
+    };
+}
+
+
+export default reduxForm({
+    form:'signin',
+    fields:['email','password']
+})(SignIn);
