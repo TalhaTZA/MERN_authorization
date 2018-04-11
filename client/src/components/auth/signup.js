23c939fb29fa1268822b69f5c63eb1c8ed11490b
TalhaTZA <talhavjtza@hotmail.com>
2018-04-11 21:50:15 +0500

    ADD default validtion logic

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -16,6 +16,7 @@ class SignUp extends Component{
             <fieldset className='form-group'>
             <label>Email:</label>
             <input className='form-control'{...email} />
+            {email.touched&&email.error&&<div className='error'>{email.error}</div>}
             </fieldset>
             <fieldset className='form-group'>
             <label>Password:</label>
@@ -25,6 +26,7 @@ class SignUp extends Component{
             <fieldset className='form-group'>
             <label>Password Confirm:</label>
             <input className='form-control' type='password' {...passwordConfirm} />
+            {passwordConfirm.touched&&passwordConfirm.error&&<div className='error'>{passwordConfirm.error}</div>}
             </fieldset>
             <button action='submit' className='btn btn-primary'>Sign UP!</button>
         </form>
@@ -34,6 +36,18 @@ class SignUp extends Component{
 
 function validate(formProps){
     const error={};
+
+    if(!formProps.email){
+        error.email='Enter a email'; 
+    }
+
+    if(!formProps.password){
+        error.password='Enter a password'; 
+    }
+
+    if(!formProps.passwordConfirm){
+        error.passwordConfirm='Enter a conformation password'; 
+    }
 
     if(formProps.password!=formProps.passwordConfirm){
         error.password='passwords must match';
