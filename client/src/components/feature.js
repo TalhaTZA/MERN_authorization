fb105a567b14b310d8eb694d335bf2b73e813811
TalhaTZA <talhavjtza@hotmail.com>
2018-10-17 22:09:01 +0500

    ADD require auth to secure routes

---
+++ client/src/components/feature.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class Feature extends Component{
+
+    render(){
+        return(
+            <div>
+                This is a feature route
+            </div>
+        );
+    };
+};
+
+export default Feature;
